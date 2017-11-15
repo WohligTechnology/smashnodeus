e59e9993f1bd3ed008ff8cc958dbf49721289d75
Chintan Shah <chintan@wohlig.com>
2017-11-15 12:06:34 +0530

    done

--- api/controllers/UploadController.js
+++ api/controllers/UploadController.js
@@ -6,33 +6,44 @@
  */
 
 module.exports = {
-    index: function (req, res) {
-        function callback2(err) {
-            Config.GlobalCallback(err, fileNames, res);
-        }
-        var fileNames = [];
-        req.file("file").upload({
-            maxBytes: 50000000 // 10 MB Storage 1 MB = 10^6
-        }, function (err, uploadedFile) {
-            if (uploadedFile && uploadedFile.length > 0) {
-                async.each(uploadedFile, function (n, callback) {
-                    Config.uploadFile(n.fd, function (err, value) {
-                        if (err) {
-                            callback(err);
-                        } else {
-                            fileNames.push(value.name);
-                            callback(null);
-                        }
-                    });
-                }, callback2);
+   index: function (req, res) {
+    function callback2(err) {
+      Config.GlobalCallback(err, fileNames, res);
+    }
+    var fileNames = [];
+    req.file("file").upload({
+      maxBytes: 50000000 // 10 MB Storage 1 MB = 10^6
+    }, function (err, uploadedFile) {
+
+      if (uploadedFile && uploadedFile.length > 0) {
+        console.log(uploadedFile);
+
+        async.each(uploadedFile, function (n, callback) {
+          if (n.filename) {
+            var splitVal = n.filename.split(".");
+            var extension = splitVal[splitVal.length - 1];
+            if (extension == "jpeg" || extension == "png" || extension == "jpg" || extension == "gif" || extension == "svg" || extension == "pdf" || extension == "xls" || extension == "xlsx" || extension == "doc") {
+              Config.uploadFile(n.fd, function (err, value) {
+                if (err) {
+                  callback(err);
+                } else {
+                  fileNames.push(value.name);
+                  callback(null);
+                }
+              });
             } else {
-                callback2(null, {
-                    value: false,
-                    data: "No files selected"
-                });
+              callback(null);
             }
+          }
+        }, callback2);
+      } else {
+        callback2(null, {
+          value: false,
+          data: "No files selected"
         });
-    },
+      }
+    });
+  },
     readFile: function (req, res) {
         Config.readUploaded(req.query.file, req.query.width, req.query.height, req.query.style, res);
     },
